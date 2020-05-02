d0773984a778030da8ab6e2607aeb66027ef43be
Shubham BHATNAGAR <54241092+Shubh0520@users.noreply.github.com>
2020-05-02 09:24:55 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,16 @@
+num = 9680206911
+
+name = 'shubham'
+
+out = 'My number is {} and My name is {}'.format(num, name)
+
+print(out)
+
+print('My number is {one} and My name is {two}, more {two}'.format(one=num, two=name))
+
+
+
+# dictionary test
+
+d = {'shubham': {'bhatnagar':[20,'may',1994]}}
+print(d['shubham']['bhatnagar'][2])
